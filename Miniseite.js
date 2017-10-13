1f5276a8e78c4832fec51bbe476e8970d84a8f7e
alinamaass <32566024+alinamaass@users.noreply.github.com>
2017-10-13 15:00:13 +0200

    Add files via upload

---
+++ Miniseite.js
@@ -0,0 +1,16 @@
+/*
+Aufgabe: 0 Arbeitsfähigkeit
+Name: Alina Maaß
+Matrikel: 256216
+Datum: 11.10.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+function alert() {
+    let eingabe = prompt("Bitte gib deinen Namen ein.", "Spongebob Schwammkopf");
+    if (eingabe != null) {
+        document.getElementById("hi").innerHTML =
+            "Hallo " + eingabe + "! Das ist eine Miniseite.";
+    }
+}
+//# sourceMappingURL=Miniseite.js.map
