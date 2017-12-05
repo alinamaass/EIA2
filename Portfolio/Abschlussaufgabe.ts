e358fbf726cce814270a03eb4b9d97b71d555876
alinamaass <32566024+alinamaass@users.noreply.github.com>
2017-12-05 21:02:18 +0100

    Add files via upload

---
+++ Portfolio/Abschlussaufgabe.ts
@@ -0,0 +1,162 @@
+ namespace Semesterarbeit {
+
+interface Bilder {
+         imgsrc : string;
+         text: string;
+         }
+     
+     var bild0 :Bilder = {
+         imgsrc: "img/pics/IMG_1856.JPG",
+         text: "Anna Wawra, September 2016",
+         }
+     
+     var bild1 :Bilder = {
+         imgsrc: "img/pics/r IMG_6834.jpg",
+         text: "Robin Maaß, Juni 2017",
+         }
+     
+     var bild2 :Bilder = {
+         imgsrc: "img/pics/IMG_1899.JPG",
+         text: "Zrmanja Wasserfälle Kroatien, Oktober 2014",
+         }
+     
+     var bild3 :Bilder = {
+         imgsrc: "img/pics/IMG_6214.JPG",
+         text: "Robin Maaß – TuS Zülpich, Oktober 2016",
+         }
+     
+     //Bilder im Array:
+     var bilderGalerie : Bilder[] = [bild0, bild1, bild2, bild3];
+     
+     
+     
+     
+//*****************while Schleife & Funktion "mittleren Umfangs"*****************
+//   *****************Weitere Funktion bei Fotogalerie.ts*****************
+     
+     
+     
+     //Ausgabe im HTML Code
+     function generatePicOverview() : void {
+        
+        var getPic : any = document.getElementById("picOverview");
+        var childgetPic : string;
+        var i: number = 0;
+        console.log("noch nicht while");
+        
+        while (i <= bilderGalerie.length) {
+        
+            console.log("while");
+            childgetPic = "";
+            childgetPic += "<div class='container'><img src='";
+            childgetPic += bilderGalerie[i].imgsrc;
+            childgetPic += "' class='pic'><div class='overlay'><div class='text'>";
+            childgetPic += bilderGalerie[i].text;
+            childgetPic += "</div></div></div>";
+         
+            getPic.innerHTML += childgetPic;
+            i++;
+            
+        }
+    
+    }
+
+     
+//   *****************Konditionale Anweisung*****************
+     
+function showNav():void{
+    
+        
+    
+        var node : any = document.getElementById("dropdown");
+        var sollSichtbarWerden : boolean = true;
+        console.log("Navi");
+        node.style.display="none";
+    
+        document.getElementById("onclick").addEventListener ("click", function(){
+            
+        if(sollSichtbarWerden == false){
+            console.log("kein Menü");
+            node.style.display="none"; 
+            sollSichtbarWerden = true;
+        }
+        else{
+            console.log("Menü");
+            node.style.display = "initial";   
+            sollSichtbarWerden = false;
+        } 
+        } ); 
+    
+    
+    document.getElementById("list").addEventListener("click", function(){
+        node.style.display = "none";
+        sollSichtbarWerden = true;
+        });
+    
+    generatePicOverview();
+    
+    
+    
+    
+    
+    
+    /*  **********Diese Funktion war für eine Aufklappfunktion bei der Galerie, ich habe mich aber für eine extra 
+        Seite als Fotogalerie entschieden, weil ein OnePager so einfach zu lang werden würde und mir 
+        dieses Konzept besser gefiel**************
+    
+    
+    //Bilder aufklappen
+        var pics: any = document.getElementById("hiddenPics");
+        var wenigerBilder : any = document.getElementById("wenigerBilder");
+        var hidden : boolean = true;
+    
+        console.log("HiddenPics");
+    
+        pics.style.display = "none";
+        wenigerBilder.style.display="none";
+    
+        document.getElementById("mehrBilder").addEventListener ("click", function(){
+        
+        if(hidden == false){
+            console.log("verstecktPics");
+            pics.style.display="none"; 
+            hidden = true;
+        }
+        else{
+            console.log("sichtbarPics");
+            pics.style.display = "initial";   
+            hidden = false;
+            document.getElementById("mehrBilder").style.display="none";
+            wenigerBilder.style.display="initial";
+        }   
+        });
+    
+        document.getElementById("wenigerBilder").addEventListener ("click", function(){
+        
+        if(hidden == false){
+            console.log("verstecktPics");
+            pics.style.display="none"; 
+            hidden = false;
+            wenigerBilder.style.display="none";
+            document.getElementById("mehrBilder").style.display="initial";
+        }
+        });
+    */
+ 
+
+}
+     
+     
+     
+     
+     document.addEventListener('DOMContentLoaded', function(){
+                showNav();
+                });
+     
+     
+     
+     
+     
+     
+     
+}
