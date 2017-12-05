64c2742036af8b0c38ff0d4a1c59d2f6ae104ce3
alinamaass <32566024+alinamaass@users.noreply.github.com>
2017-12-05 21:02:43 +0100

    Add files via upload

---
+++ Portfolio/Fotogalerie.js
@@ -0,0 +1,254 @@
+var Fotogalerie;
+(function (Fotogalerie) {
+    var bild0 = {
+        imgsrc: "img/pics/IMG_1856.JPG",
+        text: "Anna Wawra, September 2016",
+    };
+    var bild1 = {
+        imgsrc: "img/pics/r IMG_6834.jpg",
+        text: "Robin Maaß, Juni 2017",
+    };
+    var bild2 = {
+        imgsrc: "img/pics/IMG_1899.JPG",
+        text: "Zrmanja Wasserfälle Kroatien, Oktober 2014",
+    };
+    var bild3 = {
+        imgsrc: "img/pics/IMG_6214.JPG",
+        text: "Robin Maaß – TuS Zülpich, Oktober 2016",
+    };
+    var bild4 = {
+        imgsrc: "img/pics/IMG_1711.JPG",
+        text: "Schaufenstershooting für Anna Wawra Peoplefotografie, Aachen",
+    };
+    var bild5 = {
+        imgsrc: "img/pics/n IMG_6145.jpg",
+        text: "Eiszapfen, Januar 2017",
+    };
+    var bild6 = {
+        imgsrc: "img/pics/IMG_2558.JPG",
+        text: "Sonnenuntergang Düren, August 2016",
+    };
+    var bild7 = {
+        imgsrc: "img/pics/IMG_2582.JPG",
+        text: "Robin Maaß, November 2016",
+    };
+    var bild8 = {
+        imgsrc: "img/pics/IMG_2793.JPG",
+        text: "Isolalunga Italien, August 2016",
+    };
+    var bild9 = {
+        imgsrc: "img/pics/IMG_3463.JPG",
+        text: "Robin Maaß – TuS Zülpich, Oktober 2016",
+    };
+    var bild10 = {
+        imgsrc: "img/pics/n IMG_6172.jpg",
+        text: "Garten, Januar 2017",
+    };
+    var bild11 = {
+        imgsrc: "img/pics/n IMG_6182.jpg",
+        text: "Kater, November 2016",
+    };
+    var bild12 = {
+        imgsrc: "img/pics/bild12.jpg",
+        text: "September 2016, Düren",
+    };
+    var bild13 = {
+        imgsrc: "img/pics/bild13.jpg",
+        text: "Zadar, Kroatien",
+    };
+    var bild14 = {
+        imgsrc: "img/pics/bild14.jpg",
+        text: "Sonnenuntergang in Kožino, Kroatien",
+    };
+    var bild15 = {
+        imgsrc: "img/pics/bild15.jpg",
+        text: "Düren",
+    };
+    var bild16 = {
+        imgsrc: "img/pics/bild16.jpg",
+        text: "Zadar, Kroatien",
+    };
+    var bild17 = {
+        imgsrc: "img/pics/bild17.jpg",
+        text: "Feuerwerk Kölner Lichter 2017, Köln",
+    };
+    var bild18 = {
+        imgsrc: "img/pics/bild18.jpg",
+        text: "Feuerwerk Kölner Lichter 2017, Köln",
+    };
+    var bild19 = {
+        imgsrc: "img/pics/bild19.jpg",
+        text: "Kožino, Kroatien",
+    };
+    var bild20 = {
+        imgsrc: "img/pics/bild20.jpg",
+        text: "K21, Kunstsammlung Düsseldorf",
+    };
+    var bild21 = {
+        imgsrc: "img/pics/bild21.jpg",
+        text: "Sanremo Imperia, Italien",
+    };
+    var bild22 = {
+        imgsrc: "img/pics/bild22.jpg",
+        text: "Isolalunga Imperia, Italien",
+    };
+    var bild23 = {
+        imgsrc: "img/pics/bild23.jpg",
+        text: "Schweiz",
+    };
+    var bild24 = {
+        imgsrc: "img/pics/bild24.jpg",
+        text: "ALEXA Einkaufszentrum, Berlin",
+    };
+    var bild25 = {
+        imgsrc: "img/pics/bild25.jpg",
+        text: "Längste Theke der Welt, Düsseldorf",
+    };
+    var bild26 = {
+        imgsrc: "img/pics/bild26.jpg",
+        text: "Gebäude an der Rheinpromenade, Düsseldorf",
+    };
+    var bild27 = {
+        imgsrc: "img/pics/bild27.jpg",
+        text: "Sonnenuntergang Rheinufer, Düsseldorf",
+    };
+    var bild28 = {
+        imgsrc: "img/pics/bild28.jpg",
+        text: "Freilichtmuseum Kommern",
+    };
+    var bild29 = {
+        imgsrc: "img/pics/bild29.jpg",
+        text: "Clownfische im Kölner Zoo",
+    };
+    var bild30 = {
+        imgsrc: "img/pics/bild30.jpg",
+        text: "Düren",
+    };
+    var bild31 = {
+        imgsrc: "img/pics/bild31.jpg",
+        text: "RheinEnergieStadion beim Einzug des 1. FC Köln in die Europa League",
+    };
+    var bild32 = {
+        imgsrc: "img/pics/bild32.jpg",
+        text: "RheinEnergieStadion beim Einzug des 1. FC Köln in die Europa League",
+    };
+    var bild33 = {
+        imgsrc: "img/pics/bild33.jpg",
+        text: "LUST AUF GUT, Freiburg",
+    };
+    var bild34 = {
+        imgsrc: "img/pics/bild34.jpg",
+        text: "Düren",
+    };
+    var bild35 = {
+        imgsrc: "img/pics/bild35.jpg",
+        text: "Triberger Wasserfälle, Triberg",
+    };
+    var bild36 = {
+        imgsrc: "img/pics/bild36.jpg",
+        text: "Gebäude am Münsterplatz, Freiburg",
+    };
+    var bild37 = {
+        imgsrc: "img/pics/bild37.jpg",
+        text: "Basketball Länderspiel Deutschland - Frankreich im September 2015, LANXESS Arena Köln",
+    };
+    var bild38 = {
+        imgsrc: "img/pics/bild38.jpg",
+        text: "Feuerwerk Annakirmes 2016, Düren",
+    };
+    var bild39 = {
+        imgsrc: "img/pics/bild39.jpg",
+        text: "Rursee, Heimbach",
+    };
+    var bild40 = {
+        imgsrc: "img/pics/bild40.jpg",
+        text: "Robin Maaß mit dem TuS Zülpich in Leverkusen",
+    };
+    var bild41 = {
+        imgsrc: "img/pics/bild41.jpg",
+        text: "Düren",
+    };
+    var bild42 = {
+        imgsrc: "img/pics/bild42.jpg",
+        text: "Düren",
+    };
+    var bild43 = {
+        imgsrc: "img/pics/bild43.jpg",
+        text: "Doc Martens, Köln",
+    };
+    var bild44 = {
+        imgsrc: "img/pics/bild44.jpg",
+        text: "Shooting in Aachen",
+    };
+    var bild45 = {
+        imgsrc: "img/pics/bild45.jpg",
+        text: "Zrmanja Wasserfälle, Kroatien",
+    };
+    var bild46 = {
+        imgsrc: "img/pics/bild46.jpg",
+        text: "Schaufenstershooting für Anna Wawra Peoplefotografie, Aachen",
+    };
+    var bild47 = {
+        imgsrc: "img/pics/bild47.jpg",
+        text: "Shooting mit Anna Wawra",
+    };
+    var bild48 = {
+        imgsrc: "img/pics/bild48.jpg",
+        text: "iBau der Hochschule Furtwangen, Schwarzwald",
+    };
+    var bild49 = {
+        imgsrc: "img/pics/bild49.jpg",
+        text: "Düren",
+    };
+    var bild50 = {
+        imgsrc: "img/pics/bild50.jpg",
+        text: "Robin Maaß an den Triberger Wasserfällen",
+    };
+    var bild51 = {
+        imgsrc: "img/pics/bild51.jpg",
+        text: "Motocross Kleinhau",
+    };
+    //Bilder im Array:
+    var bilderGalerie = [bild0, bild1, bild2, bild3, bild4, bild5, bild6, bild7, bild8, bild9, bild10, bild11, bild12, bild13, bild14, bild15, bild16, bild17, bild18, bild19, bild20, bild21, bild22, bild23, bild24, bild25, bild26, bild27, bild28, bild29, bild30, bild31, bild32, bild33, bild34, bild35, bild36, bild37, bild38, bild39, bild40, bild41, bild42, bild43, bild44, bild45, bild46, bild47, bild48, bild49, bild50, bild51];
+    //Ausgabe im HTML Code
+    function generatePicOverview() {
+        var getPic = document.getElementById("picOverview");
+        var childgetPic;
+        var i = 0;
+        console.log("noch nicht while");
+        while (i <= bilderGalerie.length) {
+            console.log("while");
+            childgetPic = "";
+            childgetPic += "<div class='container'><img src='";
+            childgetPic += bilderGalerie[i].imgsrc;
+            childgetPic += "' class='pic'><div class='overlay'><div class='text'>";
+            childgetPic += bilderGalerie[i].text;
+            childgetPic += "</div></div></div>";
+            getPic.innerHTML += childgetPic;
+            i++;
+        }
+    }
+    function showNav() {
+        generatePicOverview();
+        var node = document.getElementById("dropdown");
+        var sollSichtbarWerden = true;
+        console.log("Navi");
+        node.style.display = "none";
+        document.getElementById("onclick").addEventListener("click", function () {
+            if (sollSichtbarWerden == false) {
+                console.log("kein Menü");
+                node.style.display = "none";
+                sollSichtbarWerden = true;
+            }
+            else {
+                console.log("Menü");
+                node.style.display = "initial";
+                sollSichtbarWerden = false;
+            }
+        });
+    }
+    document.addEventListener('DOMContentLoaded', function () {
+        showNav();
+    });
+})(Fotogalerie || (Fotogalerie = {}));
+//# sourceMappingURL=Fotogalerie.js.map
